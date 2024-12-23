e68710aa12beafb3e31c6b9e2ceae033dbdc58e4
Pablo Andres Chacon <pabloandres120297@gmail.com>
2024-12-23 13:59:19 -0500

    add validationa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MAT_FAB_DEFAULT_OPTIONS } from '@angular/material/button';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding()), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding()), 
+    provideAnimationsAsync(),
+  {provide: MAT_FAB_DEFAULT_OPTIONS, useValue: {subscriptSizing: 'dynamic'}}]
 };
--- src/app/generos/generos-crear/generos-crear.component.ts
+++ src/app/generos/generos-crear/generos-crear.component.ts
@@ -1,18 +1,42 @@
 import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { Router } from '@angular/router';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Router, RouterLink } from '@angular/router';
+import { primeraLetraMayuscula } from '../../compartidos/funciones/validaciones';
 
 @Component({
   selector: 'app-generos-crear',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, RouterLink, MatFormField, ReactiveFormsModule, MatInputModule],
   templateUrl: './generos-crear.component.html',
   styleUrl: './generos-crear.component.css'
 })
 export class GenerosCrearComponent {
   router = inject(Router);
+  private formBuilder = inject(FormBuilder);
+
+  form = this.formBuilder.group({
+    nombre: ['', {validators: [Validators.required, primeraLetraMayuscula()]}]
+  })
+
+  obtenerErrorCampoNombre(): string {
+    let nombre = this.form.controls.nombre;
+
+    if (nombre.hasError('required')) {
+      return "El campo nombre es requrido";
+    }
+
+    if (nombre.hasError('primeraLetraMayuscula')) {
+      return nombre.getError('primeraLetraMayuscula').mensaje;
+    }
+
+    return "";
+  }
   guardarCambios() {
     // ---guardar cambios
-    this.router.navigate(['generos']);
+    // this.router.navigate(['generos']);
+    console.log(this.form.value)
   }
 }
