3894a0ba797f959e6fb760e64e46bd0a3f90ddf2
Pablo Andres Chacon <pabloandres120297@gmail.com>
2025-01-22 12:49:23 -0500

    add multiple selector to gender and cinemas, create form to movies

--- src/app/peliculas/crear-pelicula/crear-pelicula.component.ts
+++ src/app/peliculas/crear-pelicula/crear-pelicula.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { PeliculaCreacionDTO } from '../peliculas';
+import { FormularioPeliculasComponent } from "../formulario-peliculas/formulario-peliculas.component";
+import { SelectorMultipleDTO } from '../../compartidos/componentes/selector-multiple/SelectroMultipleModelo';
 
 @Component({
   selector: 'app-crear-pelicula',
   standalone: true,
-  imports: [],
+  imports: [FormularioPeliculasComponent],
   templateUrl: './crear-pelicula.component.html',
   styleUrl: './crear-pelicula.component.css'
 })
 export class CrearPeliculaComponent {
-
+  generosSeleccionados: SelectorMultipleDTO[] = [];
+  generosNoSeleccionados: SelectorMultipleDTO[] = [
+    { llave: 1, valor: 'Drama' },
+    { llave: 2, valor: 'Accion' },
+    { llave: 3, valor: 'Comedia' },
+  ];
+  cinesSeleccionados: SelectorMultipleDTO[] = [];
+  cinesNoSeleccionados: SelectorMultipleDTO[] = [
+    { llave: 1, valor: 'Multicines' },
+    { llave: 2, valor: 'Acropolies' },
+    { llave: 3, valor: 'Milenium Plaza' },
+  ];
+  guardarCambios(pelicula:PeliculaCreacionDTO) {
+    console.log('creadno pelicula', pelicula);
+  }
 }
--- src/app/peliculas/editar-pelicula/editar-pelicula.component.ts
+++ src/app/peliculas/editar-pelicula/editar-pelicula.component.ts
@@ -1,13 +1,44 @@
 import { Component, Input, numberAttribute } from '@angular/core';
+import { PeliculaCreacionDTO, PeliculaDTO } from '../peliculas';
+import { FormularioPeliculasComponent } from "../formulario-peliculas/formulario-peliculas.component";
+import { SelectorMultipleDTO } from '../../compartidos/componentes/selector-multiple/SelectroMultipleModelo';
 
 @Component({
   selector: 'app-editar-pelicula',
   standalone: true,
-  imports: [],
+  imports: [FormularioPeliculasComponent],
   templateUrl: './editar-pelicula.component.html',
   styleUrl: './editar-pelicula.component.css'
 })
 export class EditarPeliculaComponent {
   @Input({ transform: numberAttribute })
   id!:number;
+
+  pelicula: PeliculaDTO = { 
+    id: 1, 
+    titulo: 'Spider-Man', 
+    trailer: 'ABC', 
+    fechaLanzamiento: new Date('2018-07-25'), 
+    poster: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Inside_Out_2_poster.jpg?20240514232832'
+  }
+
+  generosSeleccionados: SelectorMultipleDTO[] = [
+    { llave: 2, valor: 'Accion' }
+  ];
+  generosNoSeleccionados: SelectorMultipleDTO[] = [
+    { llave: 1, valor: 'Drama' },
+    { llave: 3, valor: 'Comedia' },
+  ];
+
+  cinesSeleccionados: SelectorMultipleDTO[] = [
+    { llave: 1, valor: 'Multicines' }
+  ];
+  cinesNoSeleccionados: SelectorMultipleDTO[] = [
+    { llave: 2, valor: 'Acropolies' },
+    { llave: 3, valor: 'Milenium Plaza' },
+  ];
+
+  guardarCambios(pelicula: PeliculaCreacionDTO) {
+    console.log('editando pelicula', pelicula)
+  }
 }
