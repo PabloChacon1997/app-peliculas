9305ffbfddeb4106f40818f96cf98163d8ef7930
Pablo Andres Chacon <pabloandres120297@gmail.com>
2025-02-05 12:53:08 -0500

    centralize form to edit entity

--- src/app/actores/editar-actor/editar-actor.component.ts
+++ src/app/actores/editar-actor/editar-actor.component.ts
@@ -1,42 +1,22 @@
-import { Component, inject, Input, numberAttribute, OnInit } from '@angular/core';
-import { ActorDTO, ActoresCreacionDto } from '../actores';
+import { Component, Input, numberAttribute } from '@angular/core';
 import { FormularioActoresComponent } from "../formulario-actores/formulario-actores.component";
 import { ActoresService } from '../actores.service';
-import { Router } from '@angular/router';
-import { extraerErrores } from '../../compartidos/funciones/extraerErrores';
-import { MostrarErroresComponent } from "../../compartidos/componentes/mostrar-errores/mostrar-errores.component";
-import { CargandoComponent } from "../../compartidos/componentes/cargando/cargando.component";
+import { EditarEntidadComponent } from "../../compartidos/componentes/editar-entidad/editar-entidad.component";
+import { SERVICIO_CRUD_TOKEN } from '../../compartidos/proveedores/proveedores';
 
 @Component({
   selector: 'app-editar-actor',
   standalone: true,
-  imports: [FormularioActoresComponent, MostrarErroresComponent, CargandoComponent],
+  imports: [ EditarEntidadComponent],
   templateUrl: './editar-actor.component.html',
-  styleUrl: './editar-actor.component.css'
+  styleUrl: './editar-actor.component.css',
+  providers: [
+    { provide: SERVICIO_CRUD_TOKEN, useClass: ActoresService }
+  ]
 })
-export class EditarActorComponent implements OnInit{
-  ngOnInit(): void {
-    this.actoresServices.obtenerPorId(this.id).subscribe( actor => {
-      this.actor = actor;
-    })
-  }
+export class EditarActorComponent {
+
   @Input({ transform: numberAttribute })
   id!:number;
-
-  actor!: ActorDTO;
-  actoresServices = inject(ActoresService);
-  router = inject(Router);
-  errores: string[] = [];
-
-  guardarCambios(actor: ActoresCreacionDto) {
-    this.actoresServices.actualizar(this.id, actor).subscribe({
-      next: () => {
-        this.router.navigate(['/actores']);
-      },
-      error: err => {
-        const errores = extraerErrores(err);
-        this.errores = errores;
-      }
-    })
-  }
+  formularioActores = FormularioActoresComponent;
 }
--- src/app/generos/crear-actor/crear-actor.component.ts
+++ src/app/generos/crear-actor/crear-actor.component.ts
@@ -1,17 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { FormularioActoresComponent } from "../../actores/formulario-actores/formulario-actores.component";
-import { ActoresCreacionDto } from '../../actores/actores';
 import { ActoresService } from '../../actores/actores.service';
-import { Router } from '@angular/router';
-import { extraerErrores } from '../../compartidos/funciones/extraerErrores';
-import { MostrarErroresComponent } from "../../compartidos/componentes/mostrar-errores/mostrar-errores.component";
 import { SERVICIO_CRUD_TOKEN } from '../../compartidos/proveedores/proveedores';
 import { CrearEntidadComponent } from "../../compartidos/componentes/crear-entidad/crear-entidad.component";
 
 @Component({
   selector: 'app-crear-actor',
   standalone: true,
-  imports: [FormularioActoresComponent, MostrarErroresComponent, CrearEntidadComponent],
+  imports: [ CrearEntidadComponent],
   templateUrl: './crear-actor.component.html',
   styleUrl: './crear-actor.component.css',
   providers: [
