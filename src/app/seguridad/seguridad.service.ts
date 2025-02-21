d15011eaebee01c0dbea6b0abfb563114192409f
Pablo Andres Chacon <pabloandres120297@gmail.com>
2025-02-21 17:16:45 -0500

    add admin role to users

--- src/app/seguridad/seguridad.service.ts
+++ src/app/seguridad/seguridad.service.ts
@@ -1,8 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { CredencialesUsuarioDTO, RespuestaAutenticacionDTO } from './seguridad';
+import { CredencialesUsuarioDTO, RespuestaAutenticacionDTO, UsuarioDTO } from './seguridad';
 import { Observable, tap } from 'rxjs';
+import { PaginacionDTO } from '../compartidos/modelos/PaginacionDTO';
+import { construirQueryParams } from '../compartidos/funciones/contruirQueryParams';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +17,18 @@ export class SeguridadService {
   private urlBase = environment.apiUrl + '/usuarios';
   private readonly llaveToken = 'token';
   private readonly llaveTokenExpiracion = 'token-expiracion';
+
+  obtenerUsuariosPaginados(paginacion: PaginacionDTO):Observable<HttpResponse<UsuarioDTO[]>> {
+    let queryparmas = construirQueryParams(paginacion);
+    return this.http.get<UsuarioDTO[]>(`${this.urlBase}/ListadoUsuarios`, { params: queryparmas, observe: 'response' });
+  }
+
+  hacerAdmin(email: string) {
+    return this.http.post(`${this.urlBase}/HacerAdmin`, {email});
+  }
+  eliminarAdmin(email: string) {
+    return this.http.post(`${this.urlBase}/EliminarAdmin`, {email});
+  }
 
   registrar(credenciales: CredencialesUsuarioDTO): Observable<RespuestaAutenticacionDTO> {
     return this.http.post<RespuestaAutenticacionDTO>(`${this.urlBase}/registrar`, credenciales)
@@ -43,6 +57,7 @@ export class SeguridadService {
     const token = localStorage.getItem(this.llaveToken);
     if (!token) return '';
     var dataToken = JSON.parse(atob(token.split('.')[1]));
+    console.log(dataToken)
     return dataToken[campo];
   }
 
@@ -68,6 +83,11 @@ export class SeguridadService {
   }
 
   obtenerRol(): string {
-    return '';
+    const esAdmin = this.obtenerCampoToken('esadmin');
+    if (esAdmin) {
+      return 'admin';
+    } else {
+      return '';
+    }
   }
 }
